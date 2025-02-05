56dc1d80d9202724350abae03a385852a786dfa6
Tobi <68130707+tobi437a@users.noreply.github.com>
2025-02-05 04:24:57 +0100

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,1 +1,5 @@
-print("hi")
+if game.PlaceId == 142823291 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/4ddfbd3c2dd2b3d0c0f1cd38.lua'))()
+else
+  print("hi")
+end
